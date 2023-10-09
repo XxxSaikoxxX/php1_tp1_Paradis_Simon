69518faec7f9d3c6abba7a31235d3ea2f1d9b3dd
XxxSaikoxxX <sparadis35@gmail.com>
2023-10-09 11:55:04 -0400

    validation et css

--- index.php
+++ index.php
@@ -2,27 +2,78 @@
 <html>
 <head>
     <title>Vérification de mot de passe</title>
+    <link rel="stylesheet" type="text/css" href="style.css"> <!-- Lier le fichier CSS externe -->
 </head>
 <body>
-    <h1>Vérification du mot de passe</h1>
-    <form method="post">
-        <label for="motDePasse">Mot de passe :</label>
-        <input type="password" name="motDePasse" id="motDePasse" required>
-        <br>
-        <input type="submit" value="Vérifier">
-    </form>
+    <div class="container">
+        <h1>Vérification du mot de passe</h1>
+        
+        <?php
+        session_start(); // Démarrer la session pour stocker le mot de passe enregistré
+        
+        if (empty($_SESSION['motDePasseEnregistre']) && empty($_POST['motDePasse'])) {
+            // Étape 1 : Demander à l'utilisateur de saisir et d'enregistrer le mot de passe
+        ?>
+            <form method="post">
+                <label for="motDePasse">Choisissez un mot de passe (entre 6 et 10 caractères) :</label>
+                <br>
+                <input type="password" name="motDePasse" id="motDePasse" required pattern=".{6,10}">
+                <br>
+                <input type="submit" value="Enregistrer">
+            </form>
+        <?php
+        } elseif (isset($_POST['motDePasse'])) {
+            // Étape 2 : Vérifier la longueur du mot de passe avant de l'enregistrer
+            $motDePasseEnregistre = $_POST['motDePasse'];
 
-    <?php
-    // Inclure la fonction de validation
-    include 'validation.php';
+            if (strlen($motDePasseEnregistre) < 6 || strlen($motDePasseEnregistre) > 10) {
+                echo "<p style='color: red;'>Erreur : Le mot de passe doit avoir entre 6 et 10 caractères. Réessayez.</p>";
+                // Afficher à nouveau le premier formulaire
+            ?>
+                <form method="post">
+                    <label for="motDePasse">Choisissez un mot de passe (entre 6 et 10 caractères) :</label>
+                    <br>
+                    <input type="password" name="motDePasse" id="motDePasse" required pattern=".{6,10}">
+                    <br>
+                    <input type="submit" value="Enregistrer">
+                </form>
+            <?php
+            } else {
+                // Le mot de passe a une longueur valide, enregistrez-le dans la session
+                $_SESSION['motDePasseEnregistre'] = $motDePasseEnregistre;
+                echo "Le mot de passe a été enregistré. Maintenant, veuillez le saisir à nouveau pour vérification.";
+            }
+        }
+        ?>
 
-    // Traitement du formulaire
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $motDePasseUser = $_POST['motDePasse'];
-        $validation = validerMotDePasse($motDePasseUser);
-        echo "<p>$validation</p>";
-    }
-    ?>
+        <br><br>
 
+        <?php
+        if (isset($_SESSION['motDePasseEnregistre'])) {
+            if (empty($_POST['motDePasseValide'])) {
+                // Étape 3 : Demander à l'utilisateur de saisir le mot de passe pour vérification
+            ?>
+                <form method="post">
+                    <label for="motDePasseValide">Veuillez saisir le mot de passe pour vérification :</label>
+                    <br>
+                    <input type="password" name="motDePasseValide" id="motDePasseValide" required>
+                    <br>
+                    <input type="submit" value="Vérifier">
+                </form>
+            <?php
+            } elseif (isset($_POST['motDePasseValide'])) {
+                // Étape 4 : Vérifier si le mot de passe correspond au mot de passe enregistré
+                $motDePasseSaisi = $_POST['motDePasseValide'];
+                $motDePasseEnregistre = $_SESSION['motDePasseEnregistre'];
+
+                // Inclure la fonction de validation
+                include 'validation.php';
+
+                $resultat = validerMotDePasse($motDePasseSaisi, $motDePasseEnregistre);
+                echo "<p>$resultat</p>";
+            }
+        }
+        ?>
+    </div>
 </body>
 </html>
---
+++ validation.php
@@ -0,0 +1,25 @@
+<?php
+
+function validerMotDePasse($motDePasseSaisi, $motDePasseEnregistre) {
+    // Vérifier si le mot de passe saisi correspond au mot de passe enregistré
+    if ($motDePasseSaisi === $motDePasseEnregistre) {
+        // Définir le "salt" statique
+        $salt = "ABC1234@";
+
+        // Concaténer le "salt" au mot de passe saisi
+        $motDePasseSalt = $motDePasseSaisi . $salt;
+
+        // Chiffrer le mot de passe avec SHA-1 (ou un autre algorithme de hachage de votre choix)
+        $motDePasseChiffre = sha1($motDePasseSalt);
+
+        $message = "Mot de passe correct !<br>";
+        $message .= "Mot de passe : $motDePasseSaisi<br>";
+        $message .= "Salt : $salt<br>";
+        $message .= "Mot de passe chiffré (SHA-1) : $motDePasseChiffre";
+        return $message;
+    } else {
+        return "Mot de passe incorrect. Réessayez.";
+    }
+}
+
+?>
